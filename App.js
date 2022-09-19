8276f7b8331f8db75631b21057c5d803e63bb5f9
Yuecheng Shi <yuechengshi@Yuechengs-MacBook-Pro.local>
2022-09-19 15:27:42 -0700

    add TextInput, useState

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import  Header  from './components/Header';
 
 export default function App() {
-  const name = "Awesome"
+  const name = "Awesome";
+  const[text, setText] = useState('');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on {name}!</Text>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Header>appName = {name}</Header>
+      <Text>{text}</Text>
       <StatusBar style="auto" />
     </View>
   );
