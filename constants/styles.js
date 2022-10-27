400948979f0037f0d220ea9d5ff5d4e4f0ac426e
yuechengshi <rachelshi1107@gmail.com>
2022-10-27 12:19:03 -0700

    added screens

---
+++ constants/styles.js
@@ -0,0 +1,7 @@
+export const GlobalColors = {
+    colors: {
+        slateblue: `#6a5acd`,
+        slategray: `#708090`,
+        lavender: `#e6e6fa`,
+    },
+}
---
+++ screens/AllExpenses.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet } from 'react-native';
+import ExpensesList from '../components/ExpensesList';
+
+function AllExpenses() {
+    return (
+        <ExpensesList
+            //expenses={}
+        />
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default AllExpenses;
